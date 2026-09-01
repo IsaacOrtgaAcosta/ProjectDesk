aa07201fab976ab27d3748f2391a6d0eae2e178a
IsaacOrtgaAcosta <iortga.acosta@gmail.com>
2026-09-01 16:20:52 +0200

    feat: add in memmory project creation

--- backend/app/routers/projects.py
+++ backend/app/routers/projects.py
@@ -1,11 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
 from app.schemas.project import ProjectCreate, ProjectResponse
 
-
-project_list: list[ProjectResponse] = []
 router = APIRouter(prefix="/projects", tags=["projects"])
+project_list: list[ProjectResponse] = []
 
 
-@router.get("/")
+@router.get("/", response_model=list[ProjectResponse])
 def read_projects():
     return project_list
+
+
+@router.post("/",
+             response_model=ProjectResponse,
+             status_code=status.HTTP_201_CREATED)
+def create_project(project: ProjectCreate):
+    project_id = len(project_list) + 1
+    created_project = ProjectResponse(
+        id=project_id,
+        name=project.name,
+        description=project.description,
+    )
+    project_list.append(created_project)
+    return created_project
